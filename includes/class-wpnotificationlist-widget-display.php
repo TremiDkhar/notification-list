f982dadc69383403c7e8e0a9cce896b13ac3e8b2
Tremi Dkhar <tremi@motiveinside.com>
2020-01-26 22:17:01 +0530

    Add Option whether to display only the title, or the excerpt or the full content & Archive link;

--- includes/class-wpnotificationlist-widget-display.php
+++ includes/class-wpnotificationlist-widget-display.php
@@ -35,8 +35,9 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 		$this->defaults = array(
 			'title'              => __( 'Notifications', 'wpnotificationlist' ),
 			'no_of_notification' => 10,
-			// 'show_archive'       => true,
+			'show_archive_link'  => true,
 			'order_list'         => true,
+			'display_type'       => 'the_title',
 		);
 
 		$widget_options = array(
@@ -60,8 +61,8 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 		$new_instance['title']              = sanitize_text_field( $new_instance['title'] );
 		$new_instance['no_of_notification'] = (int) $new_instance['no_of_notification'];
 		$new_instance['order_list']         = isset( $new_instance['order_list'] ) ? true : false;
-		// $new_instance['show_archive']       = isset( $new_instance['show_archive'] ) ? true : false;
-		// $new_instance['display_type']		= sanitize_text_field( $new_instance['display_type'] ); // Excerpt / Notificatin Title / Entire Notification Content
+		$new_instance['show_archive_link']  = isset( $new_instance['show_archive_link'] ) ? true : false;
+		$new_instance['display_type']       = sanitize_text_field( $new_instance['display_type'] );
 
 		return $new_instance;
 	}
@@ -76,6 +77,13 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 	public function form( $instance ) {
 
 		$instance = wp_parse_args( $instance, $this->defaults );
+
+		$display_types = array(
+			'the_title'   => 'Title Only',
+			'the_excerpt' => 'Excerpt',
+			'the_content' => 'Full Content',
+		);
+
 		?>
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:', 'wpnotificationlist' ); ?></label>
@@ -89,6 +97,20 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 			<input class="checkbox" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'order_list' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'order_list' ) ); ?>" <?php checked( $instance['order_list'] ); ?> /> 
 			<label for="<?php echo esc_attr( $this->get_field_id( 'order_list' ) ); ?>"><?php esc_html_e( 'Display in order list', 'wpnotificationlist' ); ?></label>
 		</p>
+		<p>
+			<input class="checkbox" type="checkbox" id="<?php echo esc_attr( $this->get_field_id( 'show_archive_link' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'show_archive_link' ) ); ?>" <?php checked( $instance['show_archive_link'] ); ?> />
+			<label for="<?php echo esc_attr( $this->get_field_id( 'show_archive_link' ) ); ?>"><?php esc_html_e( 'Show Archive Link', 'wpnotificationlist' ); ?></label>
+		</p>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'display_type' ) ); ?>"><?php esc_html_e( 'Display Type:', 'wpnotificationlist' ); ?></label>
+			<select class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'display_type' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'display_type' ) ); ?>">
+				<?php
+				foreach ( $display_types as $display_type => $value ) {
+					printf( '<option value="%s" %s>%s</option>', esc_attr( $display_type ), selected( $display_type, $instance['display_type'] ), esc_html( $value ) );
+				}
+				?>
+			</select>
+		</p>
 		<?php
 	}
 
@@ -101,6 +123,11 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 	 * @return void
 	 */
 	public function widget( $args, $instance ) {
+
+		// Exit if the display type is empty.
+		if ( empty( $instance['display_type'] ) ) {
+			return;
+		}
 
 		$notifications = new WP_Query(
 			array(
@@ -133,11 +160,31 @@ class WPNotificationList_Widget_Display extends WP_Widget {
 			$notifications->the_post();
 			?>
 			<li>
-				<?php the_content(); ?>
+				<?php
+				// @todo: Find a way to optimize this code for better performance.
+				switch ( $instance['display_type'] ) { // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+					case 'the_title':
+						printf( '<a href="%1$s" title="%2$s">%2$s</a>', get_the_permalink(), get_the_title() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						break;
+					case 'the_excerpt':
+						the_excerpt();
+						break;
+					case 'the_content':
+						the_content();
+						break;
+					default:
+						return;
+				}
+				?>
 			</li>
 			<?php
 		}
+
 		echo $tag['close']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+		if ( true === $instance['show_archive_link'] ) {
+			printf( '<a href="%1$s" class="wp-notification-list" title="%2$s">%2$s</a>', esc_url( get_post_type_archive_link( 'wpnotificationlist' ) ), esc_attr__( 'View All Notification', 'wpnotificationlist' ) );
+		}
 
 		echo $args['after_widget']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
